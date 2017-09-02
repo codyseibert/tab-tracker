1b4f77be5a869aecf5b91bdfb15822e3c16465ac
Cody Seibert <cseibert113@gmail.com>
2017-09-02 15:53:39 -0400

    setup of the express js server

---
+++ server/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hello world!'
+  })
+})
+
+app.listen(process.env.PORT || 8081)
